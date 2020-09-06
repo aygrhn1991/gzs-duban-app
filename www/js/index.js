9f8a16611418b643cd6cab9ad4aed82a20cbf6c6
chenyufeng <411610370@qq.com>
2020-09-06 17:48:06 +0800

    1

---
+++ www/js/index.js
@@ -0,0 +1,33 @@
+﻿var app = angular.module('app', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/page1', {
+            templateUrl: '/page/page1.html',
+            controller: 'page1Ctrl'
+        })
+        .when('/page2', {
+            templateUrl: '/page/page2.html',
+            controller: 'page2Ctrl'
+        })
+        .when('/page3', {
+            templateUrl: '/page/page3.html',
+            controller: 'page3Ctrl'
+        })
+        .when('/page4', {
+            templateUrl: '/page/page4.html',
+            controller: 'page4Ctrl'
+        })
+        .otherwise({
+            redirectTo: '/page1'
+        });
+});
+app.controller('indexCtrl', function($http, $rootScope) {});
+app.controller('page1Ctrl', function($http, $rootScope) {});
+app.controller('page2Ctrl', function($http, $scope) {
+    $http.post('https://wx2.fenglingtime.com/api/test/123').success(function(data) {
+        console.log(data);
+        $scope.data = JSON.stringify(data);
+    })
+});
+app.controller('page3Ctrl', function($http, $rootScope) {});
+app.controller('page4Ctrl', function($http, $rootScope) {});
